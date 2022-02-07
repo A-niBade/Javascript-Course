d49d63e040c41ca19c24052f60d1ce7fa55d424a
Anibal <joan.maldonadoa@gmail.com>
2022-02-07 09:58:13 -0300

    game logic done

--- guessMyNumber/app/script.js
+++ guessMyNumber/app/script.js
@@ -1,28 +1,32 @@
 'use strict';
 
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number!';
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23; 
-console.log(document.querySelector('.guess').value);
-*/
 let score = 20;
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let highscore = 0;
 
+// INIT GAME
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
+  // NO INPUT
   if (!guess) {
     document.querySelector('.message').textContent = 'No number bobo';
+
+    // PLAYER WINS
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Numero correcto!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if(score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    
+    // NUMERO ALTO
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Demasiado alto';
@@ -32,6 +36,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'Perdiste malardo';
       document.querySelector('.score').textContent = 0;
     }
+
+    // NUMERO BAJO
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Demasiado bajo';
@@ -43,3 +49,17 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+// RESET GAME
+document.querySelector('.again').addEventListener('click', function() {
+  score = 20
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem'; 
+})
