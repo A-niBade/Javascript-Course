3a1d6197cdbee5edabea1b7095d9e888c8c33380
Anibal <joan.maldonadoa@gmail.com>
2022-02-14 21:18:16 -0300

    refractored functions

--- modalWindow/app/script.js
+++ modalWindow/app/script.js
@@ -4,16 +4,19 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModals = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModals);
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsOpenModals.length; i++)
-  btnsOpenModals[i].addEventListener('click', function () {
-    console.log('BTN CLICKED');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsOpenModals[i].addEventListener('click', openModal);
 
- btnCloseModal.addEventListener('click', function () {
-   modal.classList.add('hidden');
-   overlay.classList.add('hidden');
- });
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
