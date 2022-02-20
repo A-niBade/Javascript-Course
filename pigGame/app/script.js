ccc4a76e9f4d9bf016f6ace3eed8239d5e317fae
Anibal <joan.maldonadoa@gmail.com>
2022-02-20 11:30:52 -0300

    V0.1

--- pigGame/app/script.js
+++ pigGame/app/script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 // ELEMENTS
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 const current0El = document.getElementById("current--0");
@@ -10,27 +12,77 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-// STARTING CONDITIONS
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
 
-// SCORE
-let currentScore = 0;
+// STARTING CONDITIONS
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
 
 // ROLL DICE FUNC
 btnRoll.addEventListener("click", function () {
-  // GENERATE RANDOM ROLL
-  const dice = Math.trunc(Math.random() * 6 + 1);
-  // DISPLAY DICE
-  diceEl.classList.remove("hidden");
-  diceEl.src = `img/dice-${dice}.png`;
-  // CHECK FOR ROLLED 1
-  if (dice !== 1) {
-    // ADD DICE TO SCORE
-    currentScore += dice;
-    current0El.textContent = currentScore;
-  } else {
-    // SWITCH PLAYER
+  if (playing) {
+    // GENERATE RANDOM ROLL
+    const dice = Math.trunc(Math.random() * 6 + 1);
+    // DISPLAY DICE
+    diceEl.classList.remove("hidden");
+    diceEl.src = `img/dice-${dice}.png`;
+    // CHECK FOR ROLLED 1
+    if (dice !== 1) {
+      // ADD DICE TO SCORE
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // SWITCH PLAYER
+      switchPlayer();
+    }
   }
 });
+
+// HOLD SCORE
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    // ADD SCORE TO ACTIVE PLAYER
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // CHECK SCORE >= 100
+    if (scores[activePlayer] >= 100) {
+      playing = false;
+      diceEl.classList.add("hidden");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener("click", init);
