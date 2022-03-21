49a65b4b97d75236ec33aa045db2496705dd1d49
Anibal <joan.maldonadoa@gmail.com>
2022-03-21 12:00:14 -0300

    21-03-2022

--- dataStructures/app/codeChallenge.js
+++ dataStructures/app/codeChallenge.js
@@ -92,30 +92,47 @@
 // }
 
 // CODE CHALLENGE 3
-const gameEvents = new Map([
-  [17, 'GOAL'],
-  [36, 'Substitution'],
-  [47, 'GOAL'],
-  [61, 'Substitution'],
-  [64, 'Yellow card'],
-  [69, 'Red card'],
-  [70, 'Substitution'],
-  [72, 'Substitution'],
-  [76, 'GOAL'],
-  [80, 'GOAL'],
-  [92, 'Yellow card'],
-]);
+// const gameEvents = new Map([
+//   [17, 'GOAL'],
+//   [36, 'Substitution'],
+//   [47, 'GOAL'],
+//   [61, 'Substitution'],
+//   [64, 'Yellow card'],
+//   [69, 'Red card'],
+//   [70, 'Substitution'],
+//   [72, 'Substitution'],
+//   [76, 'GOAL'],
+//   [80, 'GOAL'],
+//   [92, 'Yellow card'],
+// ]);
 
-const events = [...new Set(gameEvents.values())];
-console.log(events);
+// const events = [...new Set(gameEvents.values())];
+// console.log(events);
 
-gameEvents.delete(64);
+// gameEvents.delete(64);
 
-console.log(
-  `An event happened, on average, every ${90 / gameEvents.size} minutes`
-);
+// console.log(
+//   `An event happened, on average, every ${90 / gameEvents.size} minutes`
+// );
 
-for (const [keys, values] of gameEvents) {
-  const half = keys <= 45 ? 'First' : 'Second'
-  console.log(`${[half]} ${keys}, ${values}`);
-}
+// for (const [keys, values] of gameEvents) {
+//   const half = keys <= 45 ? 'First' : 'Second'
+//   console.log(`${[half]} ${keys}, ${values}`);
+// }
+
+// CODE CHALLENGE 4
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    const str = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+    console.log(`${str.padEnd(20)} ${'✅'.repeat(i + 1)}`);
+  }
+});
--- dataStructures/app/strings.js
+++ dataStructures/app/strings.js
@@ -51,38 +51,72 @@
 
 // compareEmails('ASDWDasdasd@gmail.com', 'ASDWDasdasd@gmail.com');
 
-const airline = 'TAP Air Portugal';
+// const airline = 'TAP Air Portugal';
 
-console.log(airline.toUpperCase());
-console.log(airline.toLowerCase());
+// console.log(airline.toUpperCase());
+// console.log(airline.toLowerCase());
 
-const passenger = 'aNiBAL';
-const passengerLower = passenger.toLowerCase();
-const passengerCorrect =
-  passengerLower[0].toUpperCase() + passengerLower.slice(1);
-console.log(passengerCorrect);
+// const passenger = 'aNiBAL';
+// const passengerLower = passenger.toLowerCase();
+// const passengerCorrect =
+//   passengerLower[0].toUpperCase() + passengerLower.slice(1);
+// console.log(passengerCorrect);
 
-const email = 'hello@ani.io';
-const loginEmail = '   Hello@Ani.IO  \n';
+// const email = 'hello@ani.io';
+// const loginEmail = '   Hello@Ani.IO  \n';
 
-const lowerEmail = loginEmail.toLowerCase();
-const trimmedEmail = lowerEmail.trim();
-console.log(trimmedEmail);
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+// console.log(trimmedEmail);
 
-const normalizedEmail = loginEmail.toLowerCase().trim();
-console.log(normalizedEmail);
-console.log(email === normalizedEmail);
+// const normalizedEmail = loginEmail.toLowerCase().trim();
+// console.log(normalizedEmail);
+// console.log(email === normalizedEmail);
 
-// REPLACING
-const priceGB = '288,97£';
-const priceUS = priceGB.replace('£', '$').replace(',', '.');
-console.log(priceUS);
+// // REPLACING
+// const priceGB = '288,97£';
+// const priceUS = priceGB.replace('£', '$').replace(',', '.');
+// console.log(priceUS);
 
-const announcement =
-  'All passengers come to barding door 23. Boarding door 23!';
-console.log(announcement.replaceAll('door', 'gate'));
+// const announcement =
+//   'All passengers come to barding door 23. Boarding door 23!';
+// console.log(announcement.replaceAll('door', 'gate'));
 
-const plane = 'Airbus A320neo';
-console.log(plane.includes('A320'));
-console.log(plane.includes('Boeing'));
-console.log(plane.startsWith('Air'));
+// const plane = 'Airbus A320neo';
+// console.log(plane.includes('A320'));
+// console.log(plane.includes('Boeing'));
+// console.log(plane.startsWith('Air'));
+
+console.log('a+very+nice+string'.split('+'));
+console.log('Jose Maldonado'.split(' '));
+
+const [firstName, lastName] = 'Jose Maldonado'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    namesUpper.push(n[0].toUpperCase() + n.slice(1));
+  }
+
+  console.log(namesUpper.join(' '));
+};
+
+capName('jessica ann smith davis');
+
+// PADDING
+const msg = 'Go to gate 23';
+console.log(msg.padStart(25, '+'));
+console.log('Anibal'.padStart(25, '+'));
+
+const maskedCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskedCreditCard(123123123123123));
