f640fee84316952ea1887f236125ea3cdfc10e63
Anibal <joan.maldonadoa@gmail.com>
2022-05-17 12:20:58 -0400

    17-05-2022

--- 11 - Advanced-DOM/app/script.js
+++ 11 - Advanced-DOM/app/script.js
@@ -56,44 +56,79 @@ message.textContent = 'We use cookies for improved functionality and analytics';
 message.innerHTML =
   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
 
-// header.prepend(message);
-header.append(message);
-// header.append(message.cloneNode(true));
-// header.before(message);
-// header.after(message);
+// // header.prepend(message);
+// header.append(message);
+// // header.append(message.cloneNode(true));
+// // header.before(message);
+// // header.after(message);
 
-// Delele elements
-document
-  .querySelector('.btn--close-cookie')
-  .addEventListener('click', () => message.remove());
+// // Delele elements
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', () => message.remove());
 
-// Styles
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
+// // Styles
+// message.style.backgroundColor = '#37383d';
+// message.style.width = '120%';
 
-console.log(message.style.backgroundColor);
-console.log(message.style.color);
+// console.log(message.style.backgroundColor);
+// console.log(message.style.color);
 
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
 
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 20 + 'px';
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 20 + 'px';
 
-document.documentElement.style.setProperty('--color-primary', 'orangered');
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
 
-// Atributes
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
+// // Atributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
 
-const link = document.querySelector('.twitter-link');
-console.log(link.getAttribute('href'));
+// const link = document.querySelector('.twitter-link');
+// console.log(link.getAttribute('href'));
 
-// Data atributes
-console.log(logo.dataset.versionNumber);
+// // Data atributes
+// console.log(logo.dataset.versionNumber);
 
-// Classes
-logo.classList.add('c');
-logo.classList.remove('c');
-logo.classList.toggle('c');
-logo.classList.contains('c'); // NOT INCLUDES
+// // Classes
+// logo.classList.add('c');
+// logo.classList.remove('c');
+// logo.classList.toggle('c');
+// logo.classList.contains('c'); // NOT INCLUDES
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', e => {
+  // const s1coords = section1.getBoundingClientRect();
+
+  // console.log(e.target.getBoundingClientRect());
+
+  // console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
+
+  // // Scrolling
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+const h1 = document.querySelector('h1');
+const alertH1 = e => {
+  alert('addEventListener: Great!');
+
+  h1.removeEventListener('mouseenter', alertH1);
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// h1.onmouseenter = e => {
+//   alert('addEventListener: Great!');
+// };
